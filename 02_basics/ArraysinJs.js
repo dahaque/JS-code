1463e973630f73982d97ee814030a0950b324479
Sadiq <161265120+dahaque@users.noreply.github.com>
2025-02-16 09:11:32 +0000

    learned more about arrays

--- 02_basics/ArraysinJs.js
+++ 02_basics/ArraysinJs.js
@@ -27,14 +27,49 @@ const myArr2 = new Array(1, 2, 3, 4,"shaktiman")
 
 // slice, splice
 
-console.log("A ", myArr);
+// console.log("A ", myArr);
 
 const myn1 = myArr.slice(1, 3) //last range is not included
 
-console.log(myn1);
-console.log("B ", myArr);
+// console.log(myn1);
+// console.log("B ", myArr);
 
 
 const myn2 = myArr.splice(1, 3) //last range is included and it changes the original array IMPORTANT
-console.log("C ", myArr);
-console.log(myn2);
+// console.log("C ", myArr);
+// console.log(myn2);
+
+//-------------------- Part 2 ------------------------
+
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+ const allHeros = marvel_heros.concat(dc_heros) // concat adds two arrays and returns a new array.
+//  console.log(allHeros);
+
+//PUSH() METHODS PUSHES ELEMENTS IN THE EXISTING ARRAY BUT THE CONCAT() METHOD RETURNS A NEW ARRAY.
+
+//We can also add multiple arrays using spread operator
+const all_new_heros = [...marvel_heros, ...dc_heros]
+
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity) //parameter is depth and can be given infinity
+// console.log(real_another_array);
+
+console.log(Array.isArray("Hitesh")) //to check if given value is array or not
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "hitesh"})) // interesting  //returns an empty array if it fails to convert it into an array
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
