b7c98b5cc2ee999ee1e2c803fa56dd877ed210cb
Sadiq <161265120+dahaque@users.noreply.github.com>
2025-03-20 09:02:31 +0000

    learned about this and basics of OOPs

---
+++ 08_OOPs/one.js
@@ -0,0 +1,40 @@
+// Here User is an onject literal (literally an object)
+const user = {
+    username: "hitesh",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+
+}
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+    return this
+}
+// new User to create new instances so the values are not overriden.
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+//console.log(userTwo);
+
+// IMP
+// this in global context gives empty object{} but only in node environment
+// but if we run this same line in browser we will get a window object.
+console.log(this);
