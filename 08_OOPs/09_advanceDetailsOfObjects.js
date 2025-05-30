6d535e1a27fb9dd45a457a52df2e278e881d7f63
Sadiq <161265120+dahaque@users.noreply.github.com>
2025-05-30 17:07:56 +0000

    Some advance stuffs about Objects in JS

---
+++ 08_OOPs/09_advanceDetailsOfObjects.js
@@ -0,0 +1,37 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+
+// THis method tells if a particular property is writeable, iterable, enumerable etc behind the scenes!
+
+// console.log(descripter);
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log("chai nhi bni");
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+// TO restrict read, write or iteration of a property in this case "name"
+Object.defineProperty(chai, 'name', {
+    //writable: false,
+    enumerable: true,
+    
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+        
+        console.log(`${key} : ${value}`);
+    }
+}
